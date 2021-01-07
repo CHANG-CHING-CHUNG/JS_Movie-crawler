b5ddefd496241ee121cc03e1f51f12fda2f41e88
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2021-01-07 21:59:56 +0800

    updated

--- test.js
+++
@@ -1,7 +0,0 @@
-const dbController = require("./dbController");
-
-dbController
-  .getAllMoviesInTheaters()
-  .then((res) => console.log(res.map((movie) => movie.name)));
-
-// dbController.findDuplicateRecords().then((res) => console.log(res));
